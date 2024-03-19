916bd90dddb0fd3b2d96f077a6ff7f645655ba69
D2Diego <dmarques1508@gmail.com>
2024-03-19 15:35:36 -0300

    Final Commit in JS

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,57 @@
-import './global.css'
-import styles from './app.module.css'
+import "./global.css";
+import styles from "./app.module.css";
 
-import { Header } from './components/Header/index'
-import { Post } from './components/post/index'
-import { useState } from 'react'
+import { Header } from "./components/Header/index";
+import { Post } from "./components/post/index";
+import { useState } from "react";
 
 function App() {
-  const [ tasks, setTasks ] = useState([]);
+  const [tasks, setTasks] = useState([]);
 
   const addPost = (text) => {
-    if(text.trim() !== ''){
-      setTasks([...tasks, { id: Date.now(), text: text, isCompleted: false }])
+    if (text.trim() !== "") {
+      setTasks([...tasks, { id: Date.now(), text: text, isCompleted: false }]);
     }
-  }
+  };
 
   const removePost = (taskId) => {
-    setTasks(tasks.filter(task => task.id !== taskId))
-  }
+    setTasks(tasks.filter((task) => task.id !== taskId));
+  };
+
+  const toggleCompleted = (taskId) => {
+    setTasks(tasks.map(task => 
+      task.id === taskId ?{ ...task, isCompleted: !task.isCompleted } : task
+      ))
+  };
+
+  const completedTasksCount = tasks.filter(task => task.isCompleted). length;
 
   return (
-  <div className={styles.page}>
-      <Header addPost={addPost}/>
+    <div className={styles.page}>
+      <Header addPost={addPost} />
 
       <div className={styles.titleBar}>
-        <div className={styles.created}>Tarefa criada <span className={styles.checked}>5</span> </div>
-        <div className={styles.completed}>Concluída <span className={styles.checked}>2 de 5</span> </div>
+        <div className={styles.created}>
+          Tarefa criada <span className={styles.checked}>{tasks.length}</span>{" "}
+        </div>
+        <div className={styles.completed}>
+          Concluída <span className={styles.checked}>{completedTasksCount} de {tasks.length}</span>{" "}
+        </div>
       </div>
 
       <div className={styles.tasks}>
-        { tasks.map(task => (
-          <Post key={task.id} text={task.text} onRemove={() => removePost(task.id)}/>
+        {tasks.map((task) => (
+          <Post
+            key={task.id}
+            text={task.text}
+            onRemove={() => removePost(task.id)}
+            toggleCompleted={() => toggleCompleted(task.id)}
+            isCompleted={task.isCompleted}
+          />
         ))}
       </div>
-  </div>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/post/index.jsx
+++ src/components/post/index.jsx
@@ -3,27 +3,25 @@ import styles from './post.module.css'
 import { FaRegCircle } from "react-icons/fa6";
 import { FaCircle } from "react-icons/fa6";
 import { FaRegTrashAlt } from "react-icons/fa";
-import { useState } from 'react';
+
 
 
 
 // eslint-disable-next-line react/prop-types
-export function Post({ text, onRemove }) {
-    const [isCompleted, setIsCompleted] = useState(false);
+export function Post({ text, onRemove, toggleCompleted, isCompleted }) {
 
-    function toggleCompleted() {
-        setIsCompleted(!isCompleted);
-    }
 
     const textStyle = isCompleted ? styles.textCompleted : '';
 
     return (
-        <div className={styles.page} onClick={toggleCompleted}>
-            {isCompleted ? <FaCircle className={styles.Icon}/> : <FaRegCircle className={styles.Icon}/>}
+        <div className={styles.page}>
+            <div onClick={toggleCompleted}>
+              {isCompleted ? <FaCircle className={styles.Icon}/> : <FaRegCircle className={styles.Icon}/>}
+            </div>
             <p className={textStyle}>
                 {text}
             </p>
-            <FaRegTrashAlt onClick={onRemove}/>
+            <FaRegTrashAlt onClick={(event) => { event.stopPropagation(); onRemove(); }}/>
         </div>
     );
 }
